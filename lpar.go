c3379ba255852b94e0e2d5a5dd5b6a3994b9e3bb
Muhammed İkinci <muhammedikinci@outlook.com>
2022-11-20 15:12:43 +0300

    Add string implementation

--- lpar.go
+++ lpar.go
@@ -1,10 +1,22 @@
 package lpar
 
+import "fmt"
+
 type Parameter map[string]interface{}
 
 func (param Parameter) With(key string, value interface{}) Parameter {
 	param[key] = value
 	return param
+}
+
+func (param Parameter) String() string {
+	parameters := ""
+
+	for key, value := range param {
+		parameters += fmt.Sprintf("%s: %v\n", key, value)
+	}
+
+	return parameters
 }
 
 func Param(key string, value interface{}) Parameter {
