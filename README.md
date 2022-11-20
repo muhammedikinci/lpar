c4e2bb1e3227be0158d2af1f7e84cc57c1557380
Muhammed İkinci <muhammedikinci@outlook.com>
2022-11-20 19:05:45 +0300

    Add struct marshal implementation

--- README.md
+++ README.md
@@ -10,19 +10,30 @@ lpar.Param("IDs", []int{1, 2}).
     With("error", errors.New("my sweet error")).
 ```
 
+Map to String & Json String
+```go
+myParameters := lpar.Param("IDs", []int{1, 2}).
+	With("is_active", true)
+
+fmt.Println(myParameters.AsString())
+fmt.Println(myParameters.AsJsonString())
+```
+
+Json Marshal for structs
+```go
+type a struct {
+    B string `json:"b"`
+}
+
+ab := a{B: "asdasd"}
+
+fmt.Println(lpar.Param("ab", ab).AsJsonString())
+```
+
 Log
 ```go
 log.Error(err, 
     lpar.Param("productID", productID).
     With("categoryID", categoryID),
 )
-```
-
-String & Json String
-```go
-myParameters := lpar.Param("IDs", []int{1, 2}).
-	With("is_active", true)
-
-fmt.Println(myParameters)
-fmt.Println(myParameters.Json())
 ```
--- lpar.go
+++ lpar.go
@@ -1,9 +1,21 @@
 package lpar
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
+	"reflect"
 	"strings"
+)
+
+const (
+	printTemplate    = "%s: %s\n"
+	variableTemplate = "%v"
+	jsonTemplate     = "\"%s\":\"%s\","
+	comma            = ","
+	leftCurlyBrace   = "{"
+	rightCurlyBrace  = "}"
+	newLine          = "\n"
+	backQuote        = "\""
 )
 
 type Parameter map[string]interface{}
@@ -19,29 +31,8 @@ func (param Parameter) With(key string, value interface{}) Parameter {
 	return param
 }
 
-const (
-	printTemplate   = "%s: %s\n"
-	stringTemplate  = "%v"
-	jsonTemplate    = "\"%s\":\"%s\","
-	comma           = ","
-	leftCurlyBrace  = "{"
-	rightCurlyBrace = "}"
-	newLine         = "\n"
-)
-
-func setPrintTemplate(key, value string) string {
-	return fmt.Sprintf(printTemplate, key, value)
-}
-
-func setStringTemplate(value interface{}) string {
-	return fmt.Sprintf(stringTemplate, value)
-}
-
-func setJsonTemplate(key, value string) string {
-	return fmt.Sprintf(jsonTemplate, key, value)
-}
-
-func (param Parameter) String() string {
+// Avoid implement string interface
+func (param Parameter) AsString() string {
 	parameters := ""
 
 	for key, value := range param {
@@ -51,7 +42,7 @@ func (param Parameter) String() string {
 	return strings.TrimRight(parameters, newLine)
 }
 
-func (param Parameter) Json() string {
+func (param Parameter) AsJsonString() string {
 	jsonString := leftCurlyBrace
 
 	for key, value := range param {
@@ -64,12 +55,38 @@ func (param Parameter) Json() string {
 func toString(value interface{}) string {
 	valueString := ""
 
-	switch valueWithType := value.(type) {
-	case bytes.Buffer:
-		valueString = valueWithType.String()
-	default:
-		valueString = setStringTemplate(valueWithType)
+	kind := reflect.TypeOf(value).Kind()
+
+	if kind == reflect.Struct {
+		typeOf := reflect.TypeOf(value)
+
+		for i := 0; i < reflect.TypeOf(value).NumField(); i++ {
+			if typeOf.Field(i).Tag.Get("json") != "" {
+				bytes, err := json.Marshal(value)
+				if err != nil {
+					return ""
+				}
+
+				valueString = fmt.Sprintf("%q", string(bytes))
+				valueString = strings.Trim(valueString, backQuote)
+				break
+			}
+		}
+	} else {
+		valueString = setVariableTemplate(value)
 	}
 
 	return valueString
+}
+
+func setPrintTemplate(key, value string) string {
+	return fmt.Sprintf(printTemplate, key, value)
+}
+
+func setVariableTemplate(value interface{}) string {
+	return fmt.Sprintf(variableTemplate, value)
+}
+
+func setJsonTemplate(key, value string) string {
+	return fmt.Sprintf(jsonTemplate, key, value)
 }
